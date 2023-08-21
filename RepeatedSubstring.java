05cdc4122a50cf5e480235b4a6a59dbcd2f04613
Elavarasu <elavarasu.subramaniam@gmail.com>
2023-08-21 18:04:28 +0530

    JavaFile

---
+++ RepeatedSubstring.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class RepeatedSubstring {
+	    public boolean repeatedSubstringPattern(String s) {
+	        for (int size=1;size<=s.length()/2;size++) {
+	            if (s.length()%size==0) {
+	                String curr=s.substring(0,size);
+	                int j=size;
+	                while (j<s.length() && s.substring(j,j+size).equals(curr)) {
+	                    j+=size;
+	                }
+	                if (j==s.length()) return true;
+	            }
+	        }
+	        return false;
+	        
+	    }
+
+}
